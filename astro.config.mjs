aa4b998fd73546b03e981ccf784c21b8bd4d3811
taroj1205 <taroj1205@gmail.com>
2024-05-10 16:46:08 +1200

    feat: duplicate the pages from v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://taroj1205.poyo.jp",
-	output: "static",
-	integrations: [sitemap()],
-	i18n: {
-		defaultLocale: "en",
-		locales: ["en", "ja"],
-	},
-	scopedStyleStrategy: "where",
-});
+  site: "https://taroj1205.poyo.jp",
+  output: "static",
+  integrations: [sitemap()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ja"],
+  },
+  scopedStyleStrategy: "attribute",
+})
