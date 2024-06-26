cf08bef620b674abec11a794531a2abdd3e810e9
taroj1205 <taroj1205@gmail.com>
2024-06-26 19:54:06 +1200

    feat(prettier): added prettier support

--- src/locales/en/translation.ts
+++ src/locales/en/translation.ts
@@ -1,361 +1,361 @@
 export const translations = {
-	footer: {
-		about: "About",
-		all_rights_reserved: "All rights reserved",
-		apps: "Apps",
-		chat: "Chat",
-		contact_info: "Contact information",
-		edit_on_crowdin: "Edit translation",
-		edit_on_github: "Edit on GitHub",
-		home: "Home",
-		name: "Shintaro Jokagi",
-		settings: "Settings",
-		sitemap: "Sitemap",
-		blog: "Blog",
-		scrollToTop: "Scroll to Top",
-		lang: "日本語",
-	},
-	header: {
-		about: "About",
-		apps: "Apps",
-		home: "Home",
-		title: "Shintaro Jokagi",
-		blog: "Blog",
-		posts: {
-			list: {
-				name: "List of Blogs",
-				description: "List of blog posts",
-			},
-			archive: {
-				name: "Blog Archives",
-				description: "Archives of blog posts",
-			},
-			hatenablog: {
-				name: "Hatena Blog",
-			},
-		},
-		chat: {
-			name: "Chat",
-			description: "Chat with other people on the internet!",
-		},
-		ncea: {
-			description: "Manage NCEA",
-		},
-		search: {
-			name: "Search",
-			description: "Search using custom Google Search.",
-		},
-		generator: {
-			name: "Image Generator",
-			description: "Image generator app",
-		},
-		analytics: {
-			name: "Analytics",
-			description: "See the analytics of this website.",
-		},
-		schedule: {
-			name: "Schedule for trip in Japan",
-			description: "See my trip plan while in Japan.",
-		},
-		"apps external": "※External links※",
-		twitter: {
-			name: "Twitter",
-		},
-		github: {
-			name: "GitHub",
-		},
-		social: "Social",
-		"try clicking": "Try clicking here!",
-		connect4: {
-			name: "Connect 4",
-			description: "Play Connect 4 with your friends!",
-		},
-		typing: {
-			name: "Typing Game",
-			description: "Learn English with typing game!",
-		},
-		uoa: {
-			name: "UoA Links",
-			description: "Useful links for UoA",
-		},
-	},
-	"language switcher": {
-		language: "Language",
-		search: "Search...",
-	},
-	skill: {
-		css: "CSS",
-		express: "Express",
-		flask: "Flask",
-		html: "HTML",
-		javascript: "JavaScript",
-		madeWith: "This website uses these:",
-		mysql: "MySQL",
-		nextjs: "Next.js",
-		nodejs: "Node.js",
-		postgres: "PostgreSQL",
-		python: "Python",
-		react: "React.js",
-		sqlite3: "SQLite3",
-		tailwind: "Tailwind",
-		title: "Skills",
-		typescript: "TypeScript",
-	},
-	projects: {
-		title: "Projects",
-		portfolio: {
-			title: "Portfolio",
-			description: "This is my portfolio site.",
-		},
-		saveTwitterLinks: {
-			title: "Save Twitter Links",
-			description:
-				"This is a chrome extension for saving twitter links you've viewed.",
-		},
-		noteRss: {
-			title: "Note Article List",
-			description:
-				"This is a RSS article list from Note for my younger brother.",
-		},
-		hideTwitterBots: {
-			title: "Hide Twitter Bots",
-			description: "This is a chrome extension for hiding twitter bots.",
-		},
-		typingGame: {
-			title: "Typing Game",
-			description: "Learn English with this typing game.",
-		},
-		connect4: {
-			title: "Connect 4",
-			description: "This is a Connect 4 game made with Next.js.",
-		},
-		typingGameUnity: {
-			title: "Typing Game (Unity)",
-			description: "A 2D typing game built with Unity.",
-		},
-		typingGameFlask: {
-			title: "Typing Game (Flask)",
-			description: "Learn English with this typing game.",
-		},
-		reversi: {
-			title: "Reversi",
-			description: "Reversi game made with Next.js",
-		},
-		chatApp: {
-			title: "Chat App",
-			description:
-				"This is a feature-rich chat app made with Next.js and Supabase.",
-		},
-	},
-	briefIntro: {
-		title: "Brief Introduction",
-		born: "Born in Japan (Tokyo) in 2005.",
-		age: "18 years old.",
-		"high school": "Graduated Westlake Boys High School in 2023 (New Zealand).",
-		university: "University of Auckland starting January 2024.",
-		bachelor:
-			"Bachelor of Science (Computer Science + Information and Technology Management)",
-		"lived in": {
-			heading: "Lived in",
-			japan: "Japan (Approx 10 years), Tokyo/Ehime",
-			philippines: "The Philippines (A year and 3 months), Cebu",
-			newzealand: "New Zealand (A year in 2017 and also since 2018), Auckland",
-		},
-		drawer: {
-			schools: {
-				title: "Been to 3 countries in 11 schools in 2023.",
-				description: "Japan, Philippines and New Zealand",
-			},
-			kendama: {
-				title: "Kendama (けん玉)",
-				description: "Japanese Traditional Toy like Yo-yo",
-			},
-			juggling: {
-				title: "Juggling",
-				description: "This is me practising juggling.",
-			},
-		},
-		hobbies: {
-			heading: "Hobbies",
-			soccer: "Football (Soccer)",
-			programming: "Programming",
-			"shorinji kempo": "Shorinji Kempo",
-			"table tennis": "Table Tennis",
-			tennis: "Tennis",
-			basketball: "Basketball",
-			volleyball: "Volleyball",
-			baseball: "Baseball",
-			softball: "Softball",
-			running: "Running",
-			"pen spinning": "Pen Spinnning",
-			badminton: "Badminton",
-		},
-		"part time job": {
-			heading: "Part-time Job",
-			westlake:
-				"Student aid for International Students at Westlake Boys High School (25th of January 2024 - 28th of February 2024)",
-		},
-	},
-	education: {
-		headings: {
-			duration: "Period",
-			end: "End",
-			grade: "Year Level",
-			location: "Location",
-			schoolName: "Name",
-			start: "Start",
-			period: "Period",
-		},
-		schoolHistory: "Academic Background",
-		schools: [
-			{
-				duration: "A year",
-				end: "05/2010",
-				grade: "1 & 2nd Year",
-				location: {
-					country: "Japan",
-					region: "Tokyo",
-				},
-				name: "Tokyo Tomonokai Setagaya Infant Life Group",
-				start: "04/2009",
-			},
-			{
-				duration: "A year",
-				end: "04/2011",
-				grade: "2nd Year",
-				location: {
-					country: "Japan",
-					region: "Tokyo",
-				},
-				name: "Kinuta Kindergarten",
-				start: "05/2010",
-			},
-			{
-				duration: "A year",
-				end: "03/2012",
-				grade: "Last (3rd) Year",
-				location: {
-					country: "Japan",
-					region: "Ehime",
-				},
-				name: "Wakakusa Kindergarten",
-				start: "05/2011",
-			},
-			{
-				duration: "2 months",
-				end: "06/2012",
-				grade: "Year 1",
-				location: {
-					country: "Japan",
-					region: "Ehime",
-				},
-				name: "Habu Elementary School",
-				start: "04/2012",
-			},
-			{
-				duration: "4.5 years",
-				end: "12/2016",
-				grade: "Year 1 to Year 5",
-				location: {
-					country: "Japan",
-					region: "Ehime",
-				},
-				name: "Hiura Elementary School",
-				start: "06/2012",
-			},
-			{
-				duration: "A year",
-				end: "12/2017",
-				grade: "Year 7",
-				location: {
-					country: "New Zealand",
-					region: "Auckland",
-				},
-				name: "Balmoral Intermediate",
-				start: "02/2017",
-			},
-			{
-				duration: "3 months",
-				end: "03/2018",
-				grade: "Year 6",
-				location: {
-					country: "Japan",
-					region: "Ehime",
-				},
-				name: "Hiura Elementary School",
-				start: "01/2018",
-			},
-			{
-				duration: "3 months",
-				end: "07/2018",
-				grade: "-",
-				location: {
-					country: "The Philippines",
-					region: "Cebu",
-				},
-				name: "Takahari English School",
-				start: "04/2018",
-			},
-			{
-				duration: "2-3 Weeks",
-				end: "07/2018",
-				grade: "Year 7",
-				location: {
-					country: "Japan",
-					region: "Ehime",
-				},
-				name: "Kuwabara Junior High School",
-				start: "07/2018",
-			},
-			{
-				duration: "A year",
-				end: "05/2019",
-				grade: "Year 8",
-				location: {
-					country: "The Philippines",
-					region: "Cebu",
-				},
-				name: "Centre for International Education British School",
-				start: "08/2018",
-			},
-			{
-				duration: "5 years",
-				end: "12/2023",
-				grade: "Year 9 to Year 13",
-				location: {
-					country: "New Zealand",
-					region: "Auckland",
-				},
-				name: "Westlake Boys High School (Expected graduation)",
-				start: "08/2018",
-			},
-		],
-	},
-	languages: {
-		heading: "Languages",
-		framework: "Framework",
-		library: "Libraries",
-		"third party": "Third-Party Services",
-	},
-	heading: {
-		programming: "Programming",
-		"open source": "Open Source",
-		projects: "Projects",
-	},
-	"open source": {
-		yamada: {
-			title: "Yamada UI (Maintainer)",
-		},
-	},
-	metadata: {
-		title: "taroj1205 - Portfolio",
-		siteName: "taroj1205's Porftolio Website",
-		description: "taroj1205's Portfolio website.",
-	},
-	notFound: {
-		title: "Page not found",
-		goBack: "Go back to Home",
-	},
-};
+  footer: {
+    about: "About",
+    all_rights_reserved: "All rights reserved",
+    apps: "Apps",
+    chat: "Chat",
+    contact_info: "Contact information",
+    edit_on_crowdin: "Edit translation",
+    edit_on_github: "Edit on GitHub",
+    home: "Home",
+    name: "Shintaro Jokagi",
+    settings: "Settings",
+    sitemap: "Sitemap",
+    blog: "Blog",
+    scrollToTop: "Scroll to Top",
+    lang: "日本語",
+  },
+  header: {
+    about: "About",
+    apps: "Apps",
+    home: "Home",
+    title: "Shintaro Jokagi",
+    blog: "Blog",
+    posts: {
+      list: {
+        name: "List of Blogs",
+        description: "List of blog posts",
+      },
+      archive: {
+        name: "Blog Archives",
+        description: "Archives of blog posts",
+      },
+      hatenablog: {
+        name: "Hatena Blog",
+      },
+    },
+    chat: {
+      name: "Chat",
+      description: "Chat with other people on the internet!",
+    },
+    ncea: {
+      description: "Manage NCEA",
+    },
+    search: {
+      name: "Search",
+      description: "Search using custom Google Search.",
+    },
+    generator: {
+      name: "Image Generator",
+      description: "Image generator app",
+    },
+    analytics: {
+      name: "Analytics",
+      description: "See the analytics of this website.",
+    },
+    schedule: {
+      name: "Schedule for trip in Japan",
+      description: "See my trip plan while in Japan.",
+    },
+    "apps external": "※External links※",
+    twitter: {
+      name: "Twitter",
+    },
+    github: {
+      name: "GitHub",
+    },
+    social: "Social",
+    "try clicking": "Try clicking here!",
+    connect4: {
+      name: "Connect 4",
+      description: "Play Connect 4 with your friends!",
+    },
+    typing: {
+      name: "Typing Game",
+      description: "Learn English with typing game!",
+    },
+    uoa: {
+      name: "UoA Links",
+      description: "Useful links for UoA",
+    },
+  },
+  "language switcher": {
+    language: "Language",
+    search: "Search...",
+  },
+  skill: {
+    css: "CSS",
+    express: "Express",
+    flask: "Flask",
+    html: "HTML",
+    javascript: "JavaScript",
+    madeWith: "This website uses these:",
+    mysql: "MySQL",
+    nextjs: "Next.js",
+    nodejs: "Node.js",
+    postgres: "PostgreSQL",
+    python: "Python",
+    react: "React.js",
+    sqlite3: "SQLite3",
+    tailwind: "Tailwind",
+    title: "Skills",
+    typescript: "TypeScript",
+  },
+  projects: {
+    title: "Projects",
+    portfolio: {
+      title: "Portfolio",
+      description: "This is my portfolio site.",
+    },
+    saveTwitterLinks: {
+      title: "Save Twitter Links",
+      description:
+        "This is a chrome extension for saving twitter links you've viewed.",
+    },
+    noteRss: {
+      title: "Note Article List",
+      description:
+        "This is a RSS article list from Note for my younger brother.",
+    },
+    hideTwitterBots: {
+      title: "Hide Twitter Bots",
+      description: "This is a chrome extension for hiding twitter bots.",
+    },
+    typingGame: {
+      title: "Typing Game",
+      description: "Learn English with this typing game.",
+    },
+    connect4: {
+      title: "Connect 4",
+      description: "This is a Connect 4 game made with Next.js.",
+    },
+    typingGameUnity: {
+      title: "Typing Game (Unity)",
+      description: "A 2D typing game built with Unity.",
+    },
+    typingGameFlask: {
+      title: "Typing Game (Flask)",
+      description: "Learn English with this typing game.",
+    },
+    reversi: {
+      title: "Reversi",
+      description: "Reversi game made with Next.js",
+    },
+    chatApp: {
+      title: "Chat App",
+      description:
+        "This is a feature-rich chat app made with Next.js and Supabase.",
+    },
+  },
+  briefIntro: {
+    title: "Brief Introduction",
+    born: "Born in Japan (Tokyo) in 2005.",
+    age: "18 years old.",
+    "high school": "Graduated Westlake Boys High School in 2023 (New Zealand).",
+    university: "University of Auckland starting January 2024.",
+    bachelor:
+      "Bachelor of Science (Computer Science + Information and Technology Management)",
+    "lived in": {
+      heading: "Lived in",
+      japan: "Japan (Approx 10 years), Tokyo/Ehime",
+      philippines: "The Philippines (A year and 3 months), Cebu",
+      newzealand: "New Zealand (A year in 2017 and also since 2018), Auckland",
+    },
+    drawer: {
+      schools: {
+        title: "Been to 3 countries in 11 schools in 2023.",
+        description: "Japan, Philippines and New Zealand",
+      },
+      kendama: {
+        title: "Kendama (けん玉)",
+        description: "Japanese Traditional Toy like Yo-yo",
+      },
+      juggling: {
+        title: "Juggling",
+        description: "This is me practising juggling.",
+      },
+    },
+    hobbies: {
+      heading: "Hobbies",
+      soccer: "Football (Soccer)",
+      programming: "Programming",
+      "shorinji kempo": "Shorinji Kempo",
+      "table tennis": "Table Tennis",
+      tennis: "Tennis",
+      basketball: "Basketball",
+      volleyball: "Volleyball",
+      baseball: "Baseball",
+      softball: "Softball",
+      running: "Running",
+      "pen spinning": "Pen Spinnning",
+      badminton: "Badminton",
+    },
+    "part time job": {
+      heading: "Part-time Job",
+      westlake:
+        "Student aid for International Students at Westlake Boys High School (25th of January 2024 - 28th of February 2024)",
+    },
+  },
+  education: {
+    headings: {
+      duration: "Period",
+      end: "End",
+      grade: "Year Level",
+      location: "Location",
+      schoolName: "Name",
+      start: "Start",
+      period: "Period",
+    },
+    schoolHistory: "Academic Background",
+    schools: [
+      {
+        duration: "A year",
+        end: "05/2010",
+        grade: "1 & 2nd Year",
+        location: {
+          country: "Japan",
+          region: "Tokyo",
+        },
+        name: "Tokyo Tomonokai Setagaya Infant Life Group",
+        start: "04/2009",
+      },
+      {
+        duration: "A year",
+        end: "04/2011",
+        grade: "2nd Year",
+        location: {
+          country: "Japan",
+          region: "Tokyo",
+        },
+        name: "Kinuta Kindergarten",
+        start: "05/2010",
+      },
+      {
+        duration: "A year",
+        end: "03/2012",
+        grade: "Last (3rd) Year",
+        location: {
+          country: "Japan",
+          region: "Ehime",
+        },
+        name: "Wakakusa Kindergarten",
+        start: "05/2011",
+      },
+      {
+        duration: "2 months",
+        end: "06/2012",
+        grade: "Year 1",
+        location: {
+          country: "Japan",
+          region: "Ehime",
+        },
+        name: "Habu Elementary School",
+        start: "04/2012",
+      },
+      {
+        duration: "4.5 years",
+        end: "12/2016",
+        grade: "Year 1 to Year 5",
+        location: {
+          country: "Japan",
+          region: "Ehime",
+        },
+        name: "Hiura Elementary School",
+        start: "06/2012",
+      },
+      {
+        duration: "A year",
+        end: "12/2017",
+        grade: "Year 7",
+        location: {
+          country: "New Zealand",
+          region: "Auckland",
+        },
+        name: "Balmoral Intermediate",
+        start: "02/2017",
+      },
+      {
+        duration: "3 months",
+        end: "03/2018",
+        grade: "Year 6",
+        location: {
+          country: "Japan",
+          region: "Ehime",
+        },
+        name: "Hiura Elementary School",
+        start: "01/2018",
+      },
+      {
+        duration: "3 months",
+        end: "07/2018",
+        grade: "-",
+        location: {
+          country: "The Philippines",
+          region: "Cebu",
+        },
+        name: "Takahari English School",
+        start: "04/2018",
+      },
+      {
+        duration: "2-3 Weeks",
+        end: "07/2018",
+        grade: "Year 7",
+        location: {
+          country: "Japan",
+          region: "Ehime",
+        },
+        name: "Kuwabara Junior High School",
+        start: "07/2018",
+      },
+      {
+        duration: "A year",
+        end: "05/2019",
+        grade: "Year 8",
+        location: {
+          country: "The Philippines",
+          region: "Cebu",
+        },
+        name: "Centre for International Education British School",
+        start: "08/2018",
+      },
+      {
+        duration: "5 years",
+        end: "12/2023",
+        grade: "Year 9 to Year 13",
+        location: {
+          country: "New Zealand",
+          region: "Auckland",
+        },
+        name: "Westlake Boys High School (Expected graduation)",
+        start: "08/2018",
+      },
+    ],
+  },
+  languages: {
+    heading: "Languages",
+    framework: "Framework",
+    library: "Libraries",
+    "third party": "Third-Party Services",
+  },
+  heading: {
+    programming: "Programming",
+    "open source": "Open Source",
+    projects: "Projects",
+  },
+  "open source": {
+    yamada: {
+      title: "Yamada UI (Maintainer)",
+    },
+  },
+  metadata: {
+    title: "taroj1205 - Portfolio",
+    siteName: "taroj1205's Porftolio Website",
+    description: "taroj1205's Portfolio website.",
+  },
+  notFound: {
+    title: "Page not found",
+    goBack: "Go back to Home",
+  },
+}
 
-export type Translation = typeof translations;
+export type Translation = typeof translations
