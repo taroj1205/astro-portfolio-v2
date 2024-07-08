13866da952e224d6a8eb1bd8dd2ee943b53edea8
taroj1205 <taroj1205@gmail.com>
2024-07-09 10:50:51 +1200

    feat(css): added purge css feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,26 @@
 import { defineConfig } from "astro/config"
 import sitemap from "@astrojs/sitemap"
-
 import playformCompress from "@playform/compress"
+
+import purgecss from "astro-purgecss"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://taroj1205.poyo.jp",
   output: "static",
-  integrations: [sitemap(), playformCompress()],
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en",
+          ja: "ja",
+        },
+      },
+    }),
+    purgecss(),
+    playformCompress(),
+  ],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ja"],
