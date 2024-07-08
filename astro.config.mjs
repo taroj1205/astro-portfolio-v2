5b9dab78fd3debe6e35c53e97f4499c98dec85b3
taroj1205 <taroj1205@gmail.com>
2024-07-08 21:00:17 +1200

    feat(compress): added third party compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config"
 import sitemap from "@astrojs/sitemap"
 
+import playformCompress from "@playform/compress"
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://taroj1205.poyo.jp",
   output: "static",
-  integrations: [sitemap()],
+  integrations: [sitemap(), playformCompress()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ja"],
