624b60b20c04f66595cf83db8bfa98590538e186
taroj1205 <taroj1205@gmail.com>
2024-07-09 11:08:14 +1200

    refactor(image): improve image sizes

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -3,17 +3,12 @@ export const generateSizesAndWidths = (
   height: number,
   minWidth = 320,
 ) => {
-  const isVertical = height > width
-  const aspectRatio = isVertical ? width / height : height / width
+  const aspectRatio = height > width ? width / height : height / width
   const widthArray = [320, 480, 640, 800, 960, 1120, 1280, 1440, 1600, 1920]
   const filteredWidthArray = widthArray.filter((w) => w >= minWidth)
   const widths = filteredWidthArray.map((w) => Math.round(w * aspectRatio))
   const sizes = filteredWidthArray.map((w) => `${w}px`).join(", ")
-  const w = isVertical
-    ? Math.round(height * aspectRatio)
-    : Math.round(width * aspectRatio)
-  const h = isVertical
-    ? Math.round(width * aspectRatio)
-    : Math.round(height * aspectRatio)
+  const w = Math.round(width * aspectRatio)
+  const h = Math.round(height * aspectRatio)
   return { width: w, height: h, widths, sizes }
 }
