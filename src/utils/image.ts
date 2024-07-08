531155b8a1dc18b5653036d203c6825444e71d45
taroj1205 <taroj1205@gmail.com>
2024-07-08 21:04:35 +1200

    feat(image): improved image sizes

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,8 +1,13 @@
-export const generateSizesAndWidths = (width: number, height: number) => {
+export const generateSizesAndWidths = (
+  width: number,
+  height: number,
+  minWidth: number,
+) => {
   const aspectRatio = height / width
   const widthArray = [320, 480, 640, 800, 960, 1120, 1280, 1440, 1600, 1920]
-  const widths = widthArray.map((w) => Math.round(w * aspectRatio))
-  const sizes = widthArray.map((w) => `${w}px`).join(", ")
+  const filteredWidthArray = widthArray.filter((w) => w >= minWidth)
+  const widths = filteredWidthArray.map((w) => Math.round(w * aspectRatio))
+  const sizes = filteredWidthArray.map((w) => `${w}px`).join(", ")
   const w = Math.round(width * aspectRatio)
   const h = Math.round(height * aspectRatio)
   return { width: w, height: h, widths, sizes }
